e100f05b2bfeffd21393a321b068e6faafc510af
yiyang186 <yiyang186@gmail.com>
2017-04-14 12:34:19 +0800

    增加绘制熵和逆转沿着垂直过载增大的方向运动轨迹的功能

--- hardlanding/helpers.py
+++ hardlanding/helpers.py
@@ -273,3 +273,16 @@ def get_all_airports_ent_opt():
         mm = dd.loc[:, ['ENTROPY', 'MIX_CROSS_RATE', 'VRTG_MAX']].mean().round(3)
         means.append(mm.values.tolist())
     return {'data': data, 'means': means, 'splits': splits}
+
+def get_ent_opt_track():
+    df = Table().get_dataFrame()
+    ff = df.loc[:, ['ENTROPY', 'MIX_CROSS_RATE', 'VRTG_MAX']].sort_values('VRTG_MAX')
+    #splits = [1, 1.05, 1.1, 1.15, 1.2, 1.25, 1.3, 1.35, 1.4, 1.45, 1.5, 1.55, 1.6, 2]
+    splits = [x/10.0 for x in range(10, 20)]
+    result = []
+    for i in range(1, len(splits)):
+        temp = ff.loc[(df['VRTG_MAX']>=splits[i-1]) & (df['VRTG_MAX']<splits[i]), :]
+        mean = temp.mean().dropna().round(4).values.tolist()
+        num = temp.shape[0]
+        result.append(mean + [num])
+    return result
--- hardlanding/views.py
+++ hardlanding/views.py
@@ -91,3 +91,8 @@ def show_all_airports_ent_opt(request):
     alldata = helpers.get_all_airports_ent_opt()
     context = {'title': '所有机场的环境熵与逆转率', 'alldata': json.dumps(alldata)}
     return render(request, 'hardlanding/ent_opt.html', context)
+
+def show_ent_opt_track(request):
+    alldata = helpers.get_ent_opt_track()
+    context = {'title': '所有机场的环境熵与逆转率质心轨迹', 'alldata': json.dumps(alldata)}
+    return render(request, 'hardlanding/track.html', context)
